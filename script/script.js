6e8a77cb645fe3d4ed83a286c983e5a47ee5e32e
Yurii Zubar <barsuk0831@gmail.com>
2020-08-17 18:57:10 +0300

    3 task

--- script/script.js
+++ script/script.js
@@ -22,17 +22,64 @@
 
 // task 2
 // Write code under this line
+// const User = function (name = "", age = 0, followers = 0) {
+//   this.name = name;
+//   this.age = age;
+//   this.followers = followers;
+// };
 
+// User.prototype.getInfo = function () {
+//   return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
+// };
 // console.log(typeof User);
-// 'function'
+// // 'function'
 
-// const mango = new User('Mango', 2, 20);
+// const mango = new User("Mango", 2, 20);
 // console.log(mango.getInfo());
-// 'User Mango is 2 years old and has 20 followers'
+// // 'User Mango is 2 years old and has 20 followers'
 
 // console.log(typeof mango.getInfo);
+// // 'function'
+
+// const poly = new User("Poly", 3, 17);
+// console.log(poly.getInfo());
+// // 'User Poly is 3 years old and has 17 followers'
+
+// task 3
+
+// Write code under this line
+// const Storage = function (items = []) {
+//   this.items = items;
+// };
+// Storage.prototype.getItems = function () {
+//   return this.items;
+// };
+
+// Storage.prototype.addItem = function (item) {
+//   return this.items.push(item);
+// };
+
+// Storage.prototype.removeItem = function (item) {
+//   if (this.items.includes(item)) {
+//     let index = this.items.indexOf(item);
+//     return this.items.splice(index, 1);
+//   }
+// };
+
+// console.log(typeof Storage);
 // 'function'
 
-// const poly = new User( 'Poly', 3, 17);
-// console.log(poly.getInfo());
-// 'User Poly is 3 years old and has 17 followers'
+// const goods = ["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+// const storage = new Storage(goods);
+
+// console.log(storage.getItems());
+//["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+// storage.addItem("Дроид");
+// console.log(storage.getItems());
+//["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид"];
+
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems());
+//["Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид"];
