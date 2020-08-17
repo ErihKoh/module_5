ab63be74f77357f9af2d346bdfd5747a3d1f4df3
Yurii Zubar <barsuk0831@gmail.com>
2020-08-17 21:46:31 +0300

    4 task

--- script/script.js
+++ script/script.js
@@ -83,3 +83,43 @@
 // storage.removeItem("Пролонгер");
 // console.log(storage.getItems());
 //["Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид"];
+
+// ?task 4
+
+// Write code under this line
+
+// class StringBuilder {
+//   constructor(value) {
+//     this._value = value;
+//   }
+//   get value() {
+//     return this._value;
+//   }
+//   append(str) {
+//     const arr = this._value.split();
+//     arr.push(str);
+//     return (this._value = arr.join(""));
+//   }
+//   prepend(str) {
+//     const arr = this._value.split();
+//     arr.unshift(str);
+//     return (this._value = arr.join(""));
+//   }
+//   pad(str) {
+//     this.append(str);
+//     this.prepend(str);
+//   }
+// }
+// console.log(typeof StringBuilder);
+// // 'function'
+
+// const builder = new StringBuilder(".");
+
+// builder.append("^");
+// console.log(builder.value); // '.^'
+
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
+
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
